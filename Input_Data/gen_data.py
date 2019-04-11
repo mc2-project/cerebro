9434b1cf05bbda125101efd733d37d1cb78db0d5
Ubuntu <ubuntu@ip-172-31-31-7.us-west-2.compute.internal>
2019-04-11 07:23:00 +0000

    run_circuit should run the circuit file now correctly. Copied off of bench_mpc. gen_data generates a 38 element list, 28 for the decision tree matrix and 10 for the client's inputs

--- Input_Data/gen_data.py
+++ Input_Data/gen_data.py
@@ -1,10 +1,12 @@
 import struct
 
 f = open("./f0", 'w')
-
-data = [123, 234]
+leaf = 1000
+data = [0, 0, 1, 2, 1, 1, 3, 4, 2, 2, 5, 6, leaf, 0, leaf, leaf, leaf, 1, leaf, leaf, leaf, 2, leaf, leaf, leaf, 3, leaf, leaf]
+test_features = [5] * 10
+data += test_features
 for d in data:
     output = struct.pack("L", d)
     f.write(output)
-
+print len(data)
 f.close()
--- emp-agmpc/test/run_circuit.cpp
+++ emp-agmpc/test/run_circuit.cpp
@@ -7,6 +7,8 @@
 #include <emp-tool/emp-tool.h>
 #include "emp-agmpc/emp-agmpc.h"
 
+
+const static int num_parties = 3;
 // This function parses the circuit input file to determine
 // which input wires belong to which party
 std::pair<uint64_t, uint64_t> parse_circuit_input(uint64_t party_id,
@@ -28,6 +30,56 @@ std::pair<uint64_t, uint64_t> parse_circuit_input(uint64_t party_id,
   return std::make_pair(num_inputs, num_outputs);
 }
 
+
+void bench_once(NetIOMP<num_parties> * ios[2], ThreadPool * pool, string filename, int party, bool *in, bool *out) {
+  if(party == 1)cout <<"CIRCUIT:\t"<<filename<<endl;
+  CircuitFile cf(filename.c_str());
+
+  auto start = clock_start();
+  CMPC<num_parties>* mpc = new CMPC<num_parties>(ios, pool, party, &cf);
+  ios[0]->flush();
+  ios[1]->flush();
+  double t2 = time_from(start);
+//  ios[0]->sync();
+//  ios[1]->sync();
+  if(party == 1)cout <<"Setup:\t"<<party<<"\t"<< t2 <<"\n"<<flush;
+
+  start = clock_start();
+  mpc->function_independent();
+  ios[0]->flush();
+  ios[1]->flush();
+  t2 = time_from(start);
+  if(party == 1)cout <<"FUNC_IND:\t"<<party<<"\t"<<t2<<" \n"<<flush;
+
+  start = clock_start();
+  mpc->function_dependent();
+  ios[0]->flush();
+  ios[1]->flush();
+  t2 = time_from(start);
+  if(party == 1)cout <<"FUNC_DEP:\t"<<party<<"\t"<<t2<<" \n"<<flush;
+
+  //bool *in = new bool[cf.n1+cf.n2]; bool *out = new bool[cf.n3];
+  memset(in, false, cf.n1+cf.n2);
+  start = clock_start();
+  mpc->online(in, out);
+  ios[0]->flush();
+  ios[1]->flush();
+  t2 = time_from(start);
+  //uint64_t band2 = ios->count();
+  //if(party == 1)cout <<"bandwidth\t"<<party<<"\t"<<band2<<endl;
+  if(party == 1)cout <<"ONLINE:\t"<<party<<"\t"<<t2<<" \n"<<flush;
+  if(party == 1) {
+    string res = "";
+    for(int i = 0; i < cf.n3; ++i)
+      res += (out[i]?"1":"0");
+    cout << "Result: " << res << endl;
+    for (int i = 0; i < cf.n3; i += 64) {
+      cout << "Result individual: " << bool_to64(out + i * 64) << endl;
+    }
+  }
+  delete mpc;
+}
+
 // This function reads the input from "filename" and sets the data in "input"
 // Each party sets a value for every input wire
 // If a party does not provide real input for a wire, the value is set to be 0
@@ -47,24 +99,41 @@ int main(int argc, char **argv) {
   }
 
   // Define some constants for the circuit setup.
-  const static int party_id = atoi(argv[1]);
-  const static int port_base  = atoi(argv[2]);
+  const static int party = atoi(argv[1]);
+  const static int port  = atoi(argv[2]);
   std::string circuit_file(argv[3]);
   std::string input_format_file(argv[4]);
   std::string input_file(argv[5]);
-  const static int num_parties = 3;
+
+
+  if (party > num_parties) {
+    return 0;
+  }
+
+  NetIOMP<num_parties> io(party, port);
+#ifdef LOCALHOST
+  NetIOMP<num_parties> io2(party, port+2*(num_parties+1)*(num_parties+1)+1);
+#else
+  NetIOMP<num_parties> io2(party, port+2*(num_parties+1));
+#endif
+  NetIOMP<num_parties> *ios[2] = {&io, &io2};
+  ThreadPool pool(2*(num_parties-1)+2);  
 
   // Input circuit file
   CircuitFile cf(circuit_file.c_str());
 
   // Parse input
   std::vector<std::pair<uint64_t, uint64_t> > input_wires;
-  std::pair<uint64_t, uint64_t> num_wires = parse_circuit_input(party_id, input_format_file, input_wires);
+  std::pair<uint64_t, uint64_t> num_wires = parse_circuit_input(party, input_format_file, input_wires);
   bool *input = new bool[num_wires.first];
   memset(input, false, num_wires.first);
   create_circuit_input(input, input_wires, input_file);
   bool *output = new bool[num_wires.second];
 
+  bench_once(ios, &pool, circuit_file, party, input, output);
+
+
+  /*
   NetIOMP<num_parties> *io_list[num_parties];
   for (uint64_t i = 0; i < num_parties; i++) {
     io_list[i] = new NetIOMP<num_parties>(party_id, port_base + i);
@@ -72,7 +141,10 @@ int main(int argc, char **argv) {
 
   ThreadPool pool(2 * num_parties);
   CMPC<num_parties>* mpc = new CMPC<num_parties>(io_list, &pool, party_id, &cf);
+  */
 
+
+  /*
   // Run the secure computation
   mpc->function_independent();
   mpc->function_dependent();
@@ -80,15 +152,19 @@ int main(int argc, char **argv) {
   int start[] = {0};
   int end[] = {(int) num_wires.first};
   mpc->online(input, output, start, end);
+  */
 
+  /*
   // Clean up memory
   for (uint64_t i = 0; i < num_parties; i++) {
     delete io_list[i];
   }
 
+
   delete input;
   delete output;
   delete mpc;
+  */
 
   return 0;
 }
