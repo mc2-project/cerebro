95a21b494b91b46e879495e6e04e9945de0f5693
Weikeng Chen <w.k@berkeley.edu>
2019-04-13 21:10:42 +0000

    add the separate input datagen script for dt

---
+++ Input_Data/gen_data_dt.py
@@ -0,0 +1,34 @@
+import struct
+import argparse
+
+def write_spdz(input_folder, data):
+    f = open(input_folder + "/f0", 'w')
+    for d in data:
+        output = struct.pack("L", d)
+        f.write(output)
+    f.close()
+
+# When writing out data for AG-MPC, we need to make sure that the format is [MSB ... LSB]
+def write_agmpc(input_folder, data):
+    f = open(input_folder + "/agmpc.input", 'w')
+    for d in data:
+        output = struct.pack(">i", d)
+        f.write(output)
+    f.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="An MC2 input parser")
+    parser.add_argument("input_folder", type=str)
+    args = parser.parse_args()
+    
+    leaf = 1000
+    data = [0, 0, 1, 2, 1, 1, 3, 4, 2, 2, 5, 6, leaf, 0, leaf, leaf, leaf, 1, leaf, leaf, leaf, 2, leaf, leaf, leaf, 3, leaf, leaf]
+    test_features = [5] * 10
+    data += test_features
+
+    # data = [1234, 2345]
+    
+    write_spdz(args.input_folder, data)
+    write_agmpc(args.input_folder, data)
+
+main()
