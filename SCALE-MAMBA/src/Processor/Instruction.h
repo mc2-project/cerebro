afd78e0733ac784859dab10485483c652833b82f
Weikeng Chen <w.k@berkeley.edu>
2019-04-17 06:35:17 +0000

    figure out the share p

--- SCALE-MAMBA/src/Processor/Instruction.h
+++ SCALE-MAMBA/src/Processor/Instruction.h
@@ -221,7 +221,7 @@ public:
 
 class Instruction : public BaseInstruction
 {
-  void execute_using_sacrifice_data(Processor &Proc,
+  void execute_using_sacrifice_data(Processor &Proc, Player &P,
                                     offline_control_data &OCD) const;
 
 public:
