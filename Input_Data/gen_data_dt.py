36475485e65c03b4ad9075eefa1953cde3d3a02b
Weikeng Chen <w.k@berkeley.edu>
2019-04-18 07:35:52 +0000

    update gen_data_dt

--- Input_Data/gen_data_dt.py
+++ Input_Data/gen_data_dt.py
@@ -1,14 +1,11 @@
 leaf = 1000
-
 data = []
-
-for i in range(10):
-    v = 2 ** i
-    for j in range(v):
-        if i == 9:
-            data += [leaf, j - v + 1, leaf, leaf]
-        else:
-            data += [i, 2, (v + j + 1) * 2 + 1, (v + j + 1) * 2 + 2]
-
-test_features = [5] * 10
+for i in range(6):
+	v = 2 ** i
+	for j in range(v):
+		if i == 5:
+			data += [leaf, j - v + 1, leaf]
+		else:
+			data += [i, 2, (v + j + 1) * 2 + 1 - (2 ** (i + 1)) - 1]
+test_features = [5] * 6
 data += test_features
