4f52dd597e77f086209718a152bba2b76543d135
Weikeng Chen <w.k@berkeley.edu>
2019-04-22 07:37:42 +0000

    add test_lr_tiny

--- emp-agmpc/emp-agmpc/cmpc_config.h
+++ emp-agmpc/emp-agmpc/cmpc_config.h
@@ -2,10 +2,14 @@
 #define __CMPC_CONFIG
 const static int abit_block_size = 1024;
 const static int fpre_threads = 1;
-#define NUM_PARTY_FOR_RUNNING 2
+#define NUM_PARTY_FOR_RUNNING 6
 const static char *IP[] = { "",
 	"172.31.39.112",
 	"172.31.41.155",
+	"172.31.44.33",
+	"172.31.34.59",
+	"172.31.47.10",
+	"172.31.32.68",
 	""};
 const static bool lan_network = false;
 #endif// __CMPC_CONFIG
