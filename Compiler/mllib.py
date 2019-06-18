bcda0d3b72eef0f96884d2718ffd8d5175ffd652
ryandeng1 <ryan.deng@berkeley.edu>
2019-06-18 00:03:48 +0000

    Edited matmul in mllib, and added in library calls to LR, ADMM, DT

--- Compiler/mllib.py
+++ Compiler/mllib.py
@@ -150,12 +150,16 @@ def _matmul_gc(A, B, C):
                 v += A[i][k] * B[k][j]
             C[i][j] = v
 
-def matmul(A, B, nparallel=1):
+def matmul(A, B, left_rows, left_cols, right_rows, right_cols, mat_type, nparallel=1):
     # Tentative, very janky. Yep, this doesn't work :(. Buyer BEWARE!
     if isinstance(A, sintMatrix) and isinstance(B, sintMatrix):
         C = sintMatrix(A.rows, B.columns)
         D = sintArray(A.rows * B.columns * A.columns)
         return _matmul(A, B, C, D, sint, nparallel)
+        #C = sintMatrix(left_rows, right_cols)
+        #D = sintArray(left_rows * right_cols * left_cols)
+        #return _matmul(A, B, C, D, sint, nparallel)
+
     elif isinstance(A, cintMatrix) and isinstance(B, cintMatrix):
         C = cintMatrix(A.rows, B.columns)
         D = cintArray(A.rows * B.columns * A.columns)
@@ -328,11 +332,19 @@ def sigmoid(v, nparallel=1):
 def mat_const_mul(c, m, nparallel=1):
     if isinstance(m, sfixMatrix):
         res = sfixMatrix(m.rows, m.columns)
-        @for_range_multithread(nparallel, m.rows, m.rows)
+        """
+        @for_range_multithread(nparallel, m.rows * m.columns, m.rows * m.columns)
         def f(i):
             @for_range_multithread(nparallel, m.columns, m.columns)
             def g(j):
                 res[i][j] = c * m[i][j]
+        """
+        @for_range_multithread(nparallel, m.rows * m.columns, m.rows * m.columns)
+        def loop(i):
+            i_index = i / m.columns
+            j_index = i % m.columns
+            res[i_index][j_index] = c * m[i_index][j_index]
+        
         return res
     elif isinstance(m, sfixMatrixGC):
         res = sfixMatrixGC(m.rows, m.columns)
@@ -537,3 +549,258 @@ class Piecewise(object):
 
         res = coefs[0] * x + coefs[1]
         return res
+
+
+
+
+def LogisticRegression(X, y, batch_size, sgd_iters, dim):
+    assert(isinstance(X, Matrix))
+    assert(isinstance(y, Matrix))
+
+    if batch_size * sgd_iters >= X.rows:
+        raise ValueError("batch_size * sgd_iters = {0} * {1} >= # of rows in X: {2}".format(batch_size, sgd_iters. X.rows))
+
+    if batch_size * sgd_iters >= y.rows:
+        raise ValueError("batch_size * sgd_iters = {0} * {1} >= # of rows in X: {2}".format(batch_size, sgd_iters. X.rows))
+
+
+    
+    if isinstance(X, sfixMatrix):
+        w = sfixMatrix(dim, 1)
+        #alpha_B = cfix(0.01 / batch_size) currently cfix and sfix multiplying doesn't work
+        alpha_B = cfix(0.01 / batch_size)
+        XB = sfixMatrix(batch_size, dim)
+        yB = sfixMatrix(batch_size, 1)
+    else:
+        w = sfixMatrixGC(dim, 1)
+        alpha_B = cfix_gc(0.01 / batch_size)
+        XB = sfixMatrixGC(batch_size, dim)
+        yB = sfixMatrixGC(batch_size, 1)
+
+    for i in range(sgd_iters):
+        batch_low = i * batch_size
+        batch_high = (i + 1) * batch_size
+
+        for j in range(batch_size):
+            for d in range(dim):
+                XB[j][d] = X[batch_low + j][d]
+            yB[j][0] = y[batch_low + j][0]
+
+        w_ret = matmul(XB, w, batch_size, dim, dim, 1, sfix)
+        #reveal_all(w_ret, "w_ret")
+        w_sigmoid = sigmoid(w_ret)
+        #reveal_all(w_sigmoid, "w_sigmoid")
+        w_sub = matsub(w_sigmoid, yB)
+        XB_T = transpose(XB)
+        w_1 = matmul(XB_T, w_sub, dim, batch_size, batch_size, 1, sfix)
+        #reveal_all(w_1, "w_1")
+        w_2 = mat_const_mul(alpha_B, w_1)
+        #reveal_all(w_2, "w_2")
+        w_res = matsub(w, w_2)
+        mat_assign(w, w_res)
+        #print_ln("Iter: %s", i)
+
+    return w
+
+
+def DecisionTree(tree, levels):
+    w = tree[0]
+    for i in range(levels-1):
+        index = w[0]
+        split = w[1]
+        left_child = w[2]
+        right_child = w[3]
+        f = x[index]
+        cond = (f < split)
+        w_res = array_index_secret_load_if(cond, tree, left_child, right_child)
+        mat_assign(w, w_res)
+
+    # Return the final prediction class.
+    return w[1]
+
+
+def get_ith_matrix(mat, index, rows, cols, mat_type=sfixMatrix):
+    #ret = s_fix_mat(rows, cols)
+    #ret = sfixMatrix(rows, cols)
+    ret = mat_type(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            ret[i][j] = mat[index * rows + i][j]
+    return ret
+
+def copy_ith_matrix(dest, src, index, rows, cols):
+    for i in range(rows):
+        for j in range(cols):
+            dest[index * rows + i][j] = src[i][j]
+
+# Local computation of weight vector.
+def admm_local(XXinv, Xy, u, z, rho, num_cols):
+    temp = matsub(z, u)
+    z_u = mat_const_mul(rho, temp)
+    #for i in range(z_u.rows):
+        #print_ln("Admm local z: %s, temp: %s", z_u[i][0].reveal(), temp[i][0].reveal())
+    second_term = matadd(Xy, z_u) #add_matrices(Xy, z_u, NUM_COLS, 1)
+    w = matmul(XXinv, second_term, num_cols, num_cols, num_cols, 1, sfix)
+    return w
+
+
+def soft_threshold_vec(threshold, vec, num_cols, mat_type=sfixMatrix):
+    #vec_new = s_fix_mat(NUM_COLS, 1)
+    #vec_new = sfixMatrix(num_cols, 1)
+    vec_new = mat_type(num_cols, 1)
+    neg_threshold = sfix(-1) * threshold
+    #neg_threshold = threshold.__neg__()
+    for i in range(num_cols):
+        threshold_fn = Piecewise(3)
+        threshold_fn.add_boundary(None, neg_threshold, sfix(0), vec[i][0] + threshold)
+        #threshold_fn.add_boundary(None, neg_threshold, c_fix(0), vec[i][0] + threshold)
+        threshold_fn.add_boundary(neg_threshold, threshold, sfix(0), sfix(0))
+        #threshold_fn.add_boundary(neg_threshold, threshold, c_fix(0), c_fix(0))
+        threshold_fn.add_boundary(threshold, None, sfix(0), vec[i][0] - threshold)
+        #threshold_fn.add_boundary(threshold, None, c_fix(0), vec[i][0] - threshold)
+        val = threshold_fn.evaluate(vec[i][0])
+        vec_new[i][0] = val
+
+    return vec_new
+
+
+
+def admm_coordinate(w_list, u_list, z, rho, l, num_cols, num_parties, mat_type=sfixMatrix):
+    #w_avg = s_fix_mat(num_cols, 1)
+    #u_avg = s_fix_mat(num_cols, 1)
+    #w_avg = sfixMatrix(num_cols, 1)
+    #u_avg = sfixMatrix(num_cols, 1)
+    w_avg = mat_type(num_cols, 1)
+    u_avg = mat_type(num_cols, 1)
+    w_avg = mat_const_mul(cfix(0), w_avg)
+    u_avg = mat_const_mul(cfix(0), u_avg)
+
+    for i in range(num_parties):
+        w = get_ith_matrix(w_list, i, num_cols, 1, mat_type)
+        u = get_ith_matrix(u_list, i, num_cols, 1, mat_type)
+        new_w_avg = matadd(w_avg, w) #add_matrices(w_avg, w, NUM_COLS, 1) 
+        new_u_avg = matadd(u_avg, u) #add_matrices(u_avg, u, NUM_COLS, 1)
+        mat_assign(w_avg, new_w_avg)
+        mat_assign(u_avg, new_u_avg)
+
+
+    #avg = c_fix(1.0 / NUM_PARTIES) cfix multiplication doesn't work
+
+    if mat_type in [sfixMatrix, sintMatrix]:
+        avg = sfix(1.0 / num_parties)  # Changing THIS line to cfix completely breaks everything wtf.
+        threshold = l / (rho * num_parties)  #sfix(l/(rho * num_parties))
+    else:
+        avg = sfix_gc(1.0 / num_parties)
+        threshold = sfix_gc(l/(rho * num_parties))
+
+    """
+    for i in range(w_avg.rows):
+        print_ln("w_avg_mul: %s, w_avg: %s", (w_avg[i][0] * cfix(1.0 / num_parties)).reveal(), w_avg[i][0].reveal())
+        print_ln("u_avg_mul: %s, u_avg: %s", (u_avg[i][0] * cfix(1.0 / num_parties)).reveal(), u_avg[i][0].reveal())
+    """
+    new_w_avg = mat_const_mul(avg, w_avg)
+    new_u_avg = mat_const_mul(avg, u_avg)
+
+    
+    mat_assign(w_avg, new_w_avg)
+    mat_assign(u_avg, new_u_avg)
+
+    # Applying thresholding
+    u_plus_w = matadd(w_avg, u_avg)
+    z_new = soft_threshold_vec(threshold, u_plus_w, num_cols, mat_type)
+    #u_list_new = s_fix_mat(num_parties * num_cols, 1)
+
+    #neg_z = s_fix_mat(num_cols, 1)
+    #u_list_new = sfixMatrix(num_parties * num_cols, 1)
+    #neg_z = sfixMatrix(num_cols, 1)
+    u_list_new = mat_type(num_parties * num_cols, 1)
+    neg_z = mat_type(num_cols, 1)    
+    for i in range(z_new.rows):
+        for j in range(z_new.columns):
+            neg_z[i][j] = z_new[i][j].__neg__()
+            
+    for i in range(num_parties):
+        u_i = get_ith_matrix(u_list, i, num_cols, 1, mat_type)
+        w_i = get_ith_matrix(w_list, i, num_cols, 1, mat_type)
+        intermediate_vec = matadd(u_i, w_i) #add_matrices(u_i, w_i, NUM_COLS, 1)
+        sum_vec = matadd(intermediate_vec, neg_z) #add_matrices(intermediate_vec, neg_z, NUM_COLS, 1)
+        copy_ith_matrix(u_list_new, sum_vec, i, num_cols, 1)
+
+    #reveal_all(z_new, "intermediate_weights")
+    return u_list_new, z_new
+
+
+def ADMM_preprocess(x_data, y_data, rho, num_parties, num_rows, num_cols, mat_type=sfixMatrix):
+    #XTX_inv_lst = s_fix_mat(NUM_PARTIES * NUM_COLS, NUM_COLS)
+    #XTy_lst = s_fix_mat(NUM_PARTIES * NUM_COLS, 1)
+    #XTX_inv_lst = sfixMatrix(num_parties * num_cols, num_cols)
+    #XTy_lst = sfixMatrix(num_parties * num_cols, 1)
+    XTX_inv_lst = mat_type(num_parties * num_cols, num_cols)
+    XTy_lst = mat_type(num_parties * num_cols, 1)
+
+    for i in range(num_parties):
+        x_i = get_ith_matrix(x_data, i, num_rows, num_cols, mat_type)
+        y_i = get_ith_matrix(y_data, i, num_rows, 1, mat_type)
+        X_T = transpose(x_i)
+        XTy = matmul(X_T, y_i, num_cols, num_rows, num_rows, 1, sfix)
+        XTX = matmul(X_T, x_i, num_cols, num_rows, num_rows, num_cols, sfix)
+
+        #rho_identity = s_fix_mat(NUM_COLS, NUM_COLS)
+        #rho_identity = sfixMatrix(num_cols, num_cols)
+        rho_identity = mat_type(num_cols, num_cols)
+        rho_identity = mat_const_mul(cfix(0), rho_identity)
+        for j in range(num_cols):
+            rho_identity[j][j] = rho #rho_val #sfix(rho_val)
+
+        XTX_rho_identity = matadd(XTX, rho_identity) #add_matrices(XTX, rho_identity, NUM_COLS, NUM_COLS)
+        XTX_inv = matinv(XTX_rho_identity)
+        copy_ith_matrix(XTX_inv_lst, XTX_inv, i, num_cols, num_cols)
+        copy_ith_matrix(XTy_lst, XTy, i, num_cols, 1)
+
+    return XTX_inv_lst, XTy_lst
+
+def ADMM(XTX_inv_lst, XTy_lst, admm_iter, num_parties, num_cols, rho, l):
+    #XTX_inv_lst, XTy_lst = local_compute(x_data, y_data, num_parties. num_rows, num_cols)
+    #w_list = s_fix_mat(num_parties * num_cols, 1)
+    mat_type = None
+    if isinstance(XTX_inv_lst, sfixMatrix):
+        mat_type = sfixMatrix 
+    elif isinstance(XTX_inv_lst, sfixMatrixGC):
+        mat_type = sfixMatrixGC
+    elif isinstance(XTX_inv_lst, sintMatrix):
+        mat_type = sintMatrix
+    else:
+        raise ValueError("Type of matrix: {0} does not correspond to anything supported by this library".format(mat_type))
+
+    #w_list = sfixMatrix(num_parties * num_cols, 1)
+    #u_list = sfixMatrix(num_parties * num_cols, 1)
+    #z = sfixMatrix(num_cols, 1)
+
+    w_list = mat_type(num_parties * num_cols, 1)
+    u_list = mat_type(num_parties * num_cols, 1)
+    z = mat_type(num_cols, 1)
+    w_list = mat_const_mul(cfix(0), w_list)
+    u_list = mat_const_mul(cfix(0), u_list)
+    z = mat_const_mul(cfix(0), z)
+    """
+    for i in range(w_list.rows):
+        for j in range(w_list.columns):
+            print_ln("%s, %s", w_list[i][j].reveal(), u_list[i][j].reveal())
+    """
+    for i in range(admm_iter):
+        for j in range(num_parties):
+            XTX_inv = get_ith_matrix(XTX_inv_lst, j, num_cols, num_cols, mat_type)
+            XTy = get_ith_matrix(XTy_lst, j, num_cols, 1, mat_type)
+            u = get_ith_matrix(u_list, j, num_cols, 1, mat_type)
+            w = admm_local(XTX_inv, XTy, u, z, rho, num_cols)
+            #reveal_all(w, "local_weight")
+            copy_ith_matrix(w_list, w, j, num_cols, 1)
+
+        new_u_lst, new_z = admm_coordinate(w_list, u_list, z, rho, l, num_cols, num_parties, mat_type)
+        mat_assign(u_list, new_u_lst)
+        mat_assign(z, new_z)
+
+    return z
+
+
+
