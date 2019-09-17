f32de104163c00d2094883635617f075fb0c7d98
Weikeng Chen <w.k@berkeley.edu>
2019-09-17 22:34:04 +0000

    debugged the hashmap generation

--- Input_Data/gen_hashmap.py
+++ Input_Data/gen_hashmap.py
@@ -5,6 +5,8 @@ import binascii
 MapNum = 11
 MapSize = 80 + 24 * 64
 
+Bound = 2 ** 64
+
 for MapId in range(1, MapNum):
     MapArray = []
     for i in range(1, MapSize):
@@ -12,5 +14,6 @@ for MapId in range(1, MapNum):
         mstr = str(MapId) + "||" + str(i)
         m.update(mstr.encode())
         mres = m.digest()
-        MapArray.append(int(binascii.hexlify(mres), 16))
-    pickle.dump(MapArray, "/home/ubuntu/mc2/Input_Data/hashmap_" + str(MapId))
+        MapArray.append(int(binascii.hexlify(mres), 16) % Bound)
+    with open("/home/ubuntu/mc2/Input_Data/hashmap_" + str(MapId), 'wb') as f:
+        pickle.dump(MapArray, f)
