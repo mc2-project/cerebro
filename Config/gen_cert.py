55d271e2861803ac28549b52f5cd031f2b09847e
Ubuntu <ubuntu@ip-172-31-11-70.us-west-2.compute.internal>
2021-06-21 01:55:06 +0000

    Change gen_cert to work with SCALE-MAMBA

--- Config/gen_cert.py
+++ Config/gen_cert.py
@@ -5,29 +5,20 @@ import argparse
 import os
 
 def main():
-    parser = argparse.ArgumentParser(description="An MC2 machine configuration loader")
-    parser.add_argument("config_file", type=str)
-    parser.add_argument("num_party", type=int)
+    parser = argparse.ArgumentParser(description="An MC2 machine configuration loader", add_help=True)
+    parser.add_argument("num_party", type=int, help="Number of parties")
     args = parser.parse_args()
     
-    # open the server list and read the list of servers
-    f_server = open(args.config_file, 'r')
-    server_list = f_server.read()
-    f_server.close()
+    # Generate certificates according to SCALE-MAMBA documentation
+    os.system("cd ./Certificates/ && openssl genrsa -out RootCA.key 4096")
+    os.system("cd ./Certificates/ && openssl req -new -x509 -days 3600 -key RootCA.key -out RootCA.crt") 
+    for party_num in range(args.num_party):
+        os.system("cd ./Certificates/ && openssl genrsa -out " + "Player{0}.key 2048".format(party_num))
+        os.system("cd ./Certificates/ && openssl req -new -key Player{0}.key -out Player{0}.csr".format(party_num))
+        os.system("cd ./Certificates/ && openssl x509 -req -days 1000 -in Player{0}.csr -CA RootCA.crt -CAkey RootCA.key -set_serial 0101 -out Player{0}.crt -sha256".format(party_num))
 
-    server_list = server_list.splitlines()
-    server_list = server_list[0:args.num_party]
 
-    os.system("cd ./Certificates/ && openssl genrsa -out ca.key 4096")
-    os.system("cd ./Certificates/ && openssl req -new -x509 -days 3600 -key ca.key -out ca.pem -subj \"/C=US/ST=California/L=Berkeley/O=RISELab/OU=MC2/CN=CA/emailAddress=w.k@berkeley.edu\"")
-   
-
-    serial_num = 0
-    for server_ip in server_list:
-        serial_num = serial_num + 1
-        os.system("cd ./Certificates/ && openssl genrsa -out " + server_ip +".key 2048")
-        os.system("cd ./Certificates/ && openssl req -new -key " + server_ip + ".key -out " + server_ip + "_presign.pem -subj \"/C=US/ST=California/L=Berkeley/O=RISELab/OU=MC2/CN=" + server_ip + "/emailAddress=w.k@berkeley.edu\"")
-        os.system("cd ./Certificates/ && openssl x509 -req -days 3000 -in " + server_ip + "_presign.pem -CA ca.pem -set_serial " + str(serial_num) + " -CAkey ca.key -out " + server_ip + ".pem -sha256")
-
+    # Copy data into SCALE-MAMBA Cert-Store
+    os.system("cp ./Certificates/* ./crypto_backend/SCALE-MAMBA/Cert-Store/")
 main()
 
